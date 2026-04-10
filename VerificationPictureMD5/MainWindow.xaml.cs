e6928be163561ed0bb2cbbe28f989b528ddf4cb3
fred <domaine75@gmail.com>
2026-04-10 11:06:33 +0200

    Ajoutez des fichiers projet.

---
+++ VerificationPictureMD5/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace VerificationPictureMD5
+{
+  /// <summary>
+  /// Logique d'interaction pour MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+  }
+}
